4b9189fba2610e5a17ec472405c15248d5adbf14
Tomasz Ostafin <ostafintomasz1@gmail.com>
2022-12-03 20:43:12 +0100

    Change day3 solutions
    Dicts aren't needed as we can simply get the priority in the list
    comprehension.

--- day3/star5.py
+++ day3/star5.py
@@ -1,10 +1,7 @@
-from typing import Dict
-from string import ascii_lowercase, ascii_uppercase
-
 with open("input", "r") as rucksacks_items:
-    letters_priorities: Dict[str, int] = {c: ord(c) - ord("a") + 1 for c in ascii_lowercase}
-    letters_priorities.update({c: ord(c) - ord("A") + 27 for c in ascii_uppercase})
     print(sum([
-        letters_priorities[(next(iter(set.intersection(s1, s2))))] for s1, s2 in
+        ord(next(iter(set.intersection(s1, s2)))) - ord("a") + 1 if
+        next(iter(set.intersection(s1, s2))).islower() else
+        ord(next(iter(set.intersection(s1, s2)))) - ord("A") + 27 for s1, s2 in
         map(lambda i: [set(i[:len(i) // 2]), set(i[len(i) // 2:])], filter(None, rucksacks_items.read().split("\n")))
     ]))
--- day3/star6.py
+++ day3/star6.py
@@ -2,9 +2,9 @@ from typing import Dict
 from string import ascii_lowercase, ascii_uppercase
 
 with open("input", "r") as rucksacks_items:
-    letters_priorities: Dict[str, int] = {c: ord(c) - ord("a") + 1 for c in ascii_lowercase}
-    letters_priorities.update({c: ord(c) - ord("A") + 27 for c in ascii_uppercase})
     print(sum([
-        letters_priorities[(next(iter(set.intersection(s1, s2, s3))))] for s1, s2, s3 in
+        ord(next(iter(set.intersection(s1, s2, s3)))) - ord("a") + 1 if
+        next(iter(set.intersection(s1, s2, s3))).islower() else
+        ord(next(iter(set.intersection(s1, s2, s3)))) - ord("A") + 27 for s1, s2, s3 in
         zip(*(iter(map(lambda i: set(i), filter(None, rucksacks_items.read().split("\n")))),) * 3)
     ]))
