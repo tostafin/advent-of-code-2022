30ef80fe890efee53952f57de6996bf1eca480c6
Tomasz Ostafin <ostafintomasz1@gmail.com>
2022-12-04 11:39:42 +0100

    Add day4 solutions

---
+++ day4/star7.py
@@ -0,0 +1,6 @@
+with open("input", "r") as elves_tasks:
+    print(len([[int(e1t1), int(e1t2), int(e2t1), int(e2t2)] for e1t1, e1t2, e2t1, e2t2 in
+               map(lambda s: s.split("-"), filter(None, elves_tasks.read().replace(",", "-").split("\n")))
+               if
+               int(e1t1) <= int(e2t1) and int(e1t2) >= int(e2t2) or int(e2t1) <= int(e1t1) and int(e2t2) >= int(e1t2)
+               ]))
