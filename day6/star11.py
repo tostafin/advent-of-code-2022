08e66b952ce44d371286b4712eb7b3a4c24d05b2
Tomasz Ostafin <ostafintomasz1@gmail.com>
2022-12-06 21:38:33 +0100

    Add day6 solutions

---
+++ day6/star11.py
@@ -0,0 +1,12 @@
+with open("input", "r") as signal:
+    markers = []
+    for i, s in enumerate(signal.read()):
+        try:
+            markers = markers[markers.index(s) + 1:]
+        except ValueError:
+            pass
+
+        markers.append(s)
+        if len(markers) == 4:
+            print(i + 1)
+            break
